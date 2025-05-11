4f9f17bd926ff51f9aa3133e51d107d419679f2a
Monica@Ideapad-tasya <monicasyatasya@gmail.com>
2025-05-11 09:31:25 +0700

    coba nambahin js

--- main.js
+++ main.js
@@ -1,13 +1,56 @@
 function ringkas(){
     const input = document.getElementById("inputText").value.trim();
 
-    if(input === " ") return;
+    if(input === "") return;
 
     const summary= input;
     document.getElementById("hasilRingkasan").innerText = summary;
+    
+    let history = JSON.parse(localStorage.getItem("summaryHistory")) || [];
+    history.push(summary);
+    localStorage.setItem("summaryHistory", JSON.stringify(history))
+
+    renderHistory();
 }
 
 function reset(){
-    document.getElementById("inputText").value= " ";
+    document.getElementById("inputText").value= "";
     document.getElementById("hasilRingkasan").innerText = "belum ada teks";
 }
+
+function renderHistory(){
+    const riwayatList= document.getElementById("riwayat");
+    riwayatList.innerHTML="";
+
+    let history = JSON.parse(localStorage.getItem("summaryHistory"))|| [];
+   
+    if(history.length === 0)
+        return (riwayatList.innerHTML = "Belum ada riwayat ringkasan");
+
+    history.forEach((item, index) => {
+        let li = document.createElement("li");
+        li.className= "flex justify-between items-center";
+        li.innerText= item;
+
+        let delButton = document.createElement("button");
+        delButton.className="bg-cyan-400 px-[10px] my-[5px] rounded-md hover:bg-red-500 transition duration-300"
+        delButton.innerHTML= "Delete";
+
+        delButton.onclick= function(){
+            deleteHistory(index);
+        };
+        
+        li.appendChild(delButton);
+        riwayatList.appendChild(li);
+
+    });
+}
+
+function deleteHistory(index){
+    let history= JSON.parse(localStorage.getItem("summaryHistory")) || [];
+    history.splice(index,1);
+
+    localStorage.setItem("summaryHistory", JSON.stringify(history));
+    renderHistory();
+}
+window.onload = renderHistory;
